4fa2afa3c86a9d618f70ededbd67f23e51627fdb
pblaas <patrick@kite4fun.nl>
2016-07-09 10:36:49 +0200

    update index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "Good Morning Zwolle";
-echo "Container: " $_SERVER['SERVER_NAME'];
+echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
+echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 ?>
 
 
