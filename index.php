405248b1b4cded762bbce90c61b4bd5309b677e0
pblaas <patrick@kite4fun.nl>
2016-07-09 10:42:28 +0200

    update index

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
-phpinfo();
-
+echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
 ?>
 
 
