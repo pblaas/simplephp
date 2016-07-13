c71e0ee158b5abbb1ca6707fc9701494aa426877
Patrick <pblaas@users.noreply.github.com>
2016-07-13 14:21:25 +0200

    update index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
-echo "Welcome in Rotterdam";
+echo "Welcome to the future.";
 ?>
 
 
