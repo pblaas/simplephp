d0f711142af16d74ed6f8e705706c42b2422a9f8
Patrick <pblaas@users.noreply.github.com>
2016-09-16 15:04:28 +0200

    Update index.php
    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
-echo "Welcome to the the rain.";
+echo "Dit is een test.";
 ?>
 
 
