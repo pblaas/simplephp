410c47691c125c7226bc72060a39bab28d536e1f
Patrick <pblaas@users.noreply.github.com>
2017-05-04 23:01:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
-echo "Dit is een maart 2017 - 2";
+echo "Dit is een mei 05 2017 - 2";
 ?>
 
 
