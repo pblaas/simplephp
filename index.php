941042f77ca009d4372427ee4474abd115512ff2
pblaas <pblaas@it-ernity.nl>
2017-05-06 17:01:19 +0200

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
-echo "Dit is een mei 06 2017 3";
+echo "jenkins pipelines";
 ?>
 
 
