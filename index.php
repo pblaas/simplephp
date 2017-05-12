68c641485bd29ae8f50c680edf8032784c43a6c8
pblaas <pblaas@it-ernity.nl>
2017-05-12 13:59:37 +0200

    update

--- index.php
+++ index.php
@@ -3,6 +3,5 @@ echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
 echo "12 mei 2017";
-?>
 
 
